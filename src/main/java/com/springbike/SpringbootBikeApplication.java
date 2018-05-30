da83905f0bcfe5da799e9d4421d63fcfeecb55a1
Suhag Raval <suhag04@hotmail.com>
2018-05-30 14:17:46 -0400

    updated SpringbootBikeApplication

--- src/main/java/com/springbike/SpringbootBikeApplication.java
+++ src/main/java/com/springbike/SpringbootBikeApplication.java
@@ -2,11 +2,17 @@ package com.springbike;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootBikeApplication {
+public class SpringbootBikeApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBikeApplication.class, args);
+	}
+	
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringbootBikeApplication.class);
 	}
 }
